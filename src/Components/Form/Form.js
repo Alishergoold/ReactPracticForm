f95839dd5fb739901c759d34029c6c56b4d8fe47
Alishergoold <alishergoold1989@gmail.com>
2021-06-25 21:29:51 +0500

    Checkbox,

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,29 +1,92 @@
 import React, { Component } from 'react';
 import {Button, Form} from 'react-bootstrap';
+import Input from './Input';
 
 class SignIn extends Component {
-  render() {
-    return (   
-      <Form>
-          <Form.Group controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
-          </Form.Group>
+  state ={
+    user: {
+      email:'',
+      password:'',
+      isAgree: null,
+    }
+  }
 
-          <Form.Group controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-          </Form.Group>
-          <Form.Group controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
-          </Form.Group>
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form>
+  inputHandler = (e) => {
+    const {name,value} = e.target;
+    //console.log(e);
+    //const{user} = this.state;
+    //this.setState({ user: {...user, [name]: value } });
+    this.setState((prevState)=>({
+      ...prevState, user: {...prevState.user,[name]: value},
+    }))
+    //console.log(name, value, this.state);
+      
+  }
+
+  checkBoxHandler = (e) => {
+    const{checked, name} = e.target;
+    this.setState((prevState)=>({
+      ...prevState, user: {...prevState.user,[name]: checked}
+    }))
+  }
+
+  submitHandler = (e) => {
+    e.preventDefault();
+    console.log('form submitted', this.state.user);
+  }
+
+  render() {
+    const {email, password, isAgree} = this.state;
+    return (   
+      <Form onSubmit={this.submitHandler}>
+        <Input 
+          name="email" 
+          value={email}
+          handler={this.inputHandler}
+          type="email"
+          label="Enter you email"
+        />
+        <Input 
+          name="password" 
+          value={password}
+          handler={this.inputHandler}
+          type="password"
+          label="Enter you password"
+        />
+        <Input 
+          name="isAgree" 
+          value={isAgree}
+          handler={this.checkBoxHandler}
+          type="checkbox"
+          label="Do you agree."
+          isCheckbox
+        />
+
+        {/*<Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control 
+            onChange={this.inputHandler} 
+            name="password"
+            type="password" 
+            placeholder="Password"
+            value={password}
+          />
+        </Form.Group>*/}
+
+        {/*<Form.Group controlId="formBasicCheckbox">
+          <Form.Check 
+            onChange={this.checkBoxHandler} 
+            type="checkbox" 
+            name = "isAgree"
+            checked = {isAgree}
+            label="Do you agree."
+          />
+        </Form.Group>*/}
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
     );
   }
 }
--- src/Components/Form/Input.js
+++ src/Components/Form/Input.js
@@ -1,1 +1,37 @@
+import React from 'react';
+import {Form} from 'react-bootstrap';
 
+const  Input = ({email, name, type, label, handler, isCheckbox, value}) => {
+    if(isCheckbox){
+      return (
+        <Form.Group controlId="formBasicCheckbox">
+          <Form.Check 
+            onChange={handler} 
+            type={type} 
+            name = {name}
+            checked = {value}
+            label= {label}
+          />
+        </Form.Group>
+      );
+    }else{
+      return (
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>
+            {label}
+          </Form.Label>
+          <Form.Control 
+            onChange={handler} 
+            type={type}
+            name={name}
+            value={value}
+            placeholder={label}
+            
+         />
+        </Form.Group>
+      );
+    }
+
+}
+
+export default Input;
