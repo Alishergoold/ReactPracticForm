a0ad55726e4aad0238171c41b0d7a802a74f5741
Alishergoold <alishergoold1989@gmail.com>
2021-06-25 15:12:14 +0500

    add Bootstrap

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Form from './Components/Form/Form';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+    return (
+      <div className="form">
+        <Form />
+      </div>
+    );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './style.css';
-import App from './Components/App.js';
-import Note from './Components/Note.js';
+import App from './App';
+
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <Note />
   </React.StrictMode>,
   document.getElementById('root')
 );
